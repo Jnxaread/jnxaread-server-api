7dae1e5ea053b7ca0923287c5573467ef92b5476
李嵩 <lisong@pansoft.com>
2020-04-21 16:33:27 +0800

    初始化提交

---
+++ src/main/java/com/jnxaread/JnxareadServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.jnxaread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnxareadServerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnxareadServerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jnxaread/JnxareadServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnxaread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnxareadServerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
