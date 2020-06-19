db2db51c916673c89c4ba04c0e645aa497e67e6a
李嵩 <lisong@pansoft.com>
2020-06-19 17:11:46 +0800

    设置工程启动时开启定时任务

--- src/main/java/com/jnxaread/JnxareadServerApiApplication.java
+++ src/main/java/com/jnxaread/JnxareadServerApiApplication.java
@@ -2,8 +2,11 @@ package com.jnxaread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//开启定时任务
+@EnableScheduling
 public class JnxareadServerApiApplication {
 
     public static void main(String[] args) {
